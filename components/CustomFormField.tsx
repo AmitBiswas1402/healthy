c781dd93579a63482b4a5897af3c9338d07539d6
AmitBiswas1402 <amit.142biswas@gamil.com>
2024-11-04 19:04:30 +0530

    it sucks now

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -11,6 +11,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Control } from "react-hook-form";
 import { FormFieldType } from "./forms/PatientForm";
+import Image from "next/image";
 
 interface CustomProps {
   control: Control<any>;
@@ -27,16 +28,40 @@ interface CustomProps {
   fieldType: FormFieldType;
 }
 
-const renderInput = () => {
-  return (
-    <Input
-      type="text" 
-      placeholder="Your name"
-    />
-  )
+const RenderField = ({field, props}: {field: any; props: CustomProps}) => {
+  const {fieldType, iconAlt, iconSrc, placeholder} = props;
+
+  switch (fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className="flex rounded-md border border-dak-500 bg-dark-400">
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              height={24}
+              width={24}
+              alt={iconAlt || 'icon'}
+              className="ml-2"
+            />
+          )}
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              {...field}
+              className="shad-input border-0"
+            />
+          </FormControl>
+        </div>
+      )
+  
+    default:
+      break;
+  }
 }
 
-const CustomFormField = ({control, fieldType, name, label}: CustomProps) => {
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, name, label } = props;
+
   return (
     <FormField
       control={control}
@@ -46,6 +71,10 @@ const CustomFormField = ({control, fieldType, name, label}: CustomProps) => {
           {fieldType !== FormFieldType.CHECKBOX && label && (
             <FormLabel>{label}</FormLabel>
           )}
+
+          <RenderField field={field} props={props} />
+
+          <FormMessage className="shad-error" />
         </FormItem>
       )}
     />
